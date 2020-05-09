e0f674d392961975c3c1fa4c818dc1d21da18fd1
Eric  Ou <eric.ou@yale.edu>
2020-05-09 07:56:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,12 +50,12 @@ end
 #####################################################
 
 def runner
-  welcome()
-  # card_total = initial_round
-  # until card_total > 21 do
-  #   card_total = hit?(card_total)
-  #   display_card_total(card_total)
-  # end 
-  # end_game
+  welcome
+  card_total = initial_round
+  until card_total > 21 do
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end 
+  end_game
 end
     
